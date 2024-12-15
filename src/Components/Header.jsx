bd23fe053b534dc7432d11d2a429080f0799401b
ifnavneetrajput <navneetrajput404@gmail.com>
2024-12-15 19:18:27 +0530

    episode 9 optimization

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import LOGO_URL from "../../utils/constant";
 import { Link } from "react-router-dom";
 
+import useonlineStatus from "../../utils/useOnlineStatus";
+
 const Header = ()=>{
 
   const [btnNameReact ,setbtnNameReact] =useState("Login");
+
+  const onlineStatus = useonlineStatus();
   return(
    <div className ="header">
     <div className="logo-container">
@@ -13,9 +17,11 @@ const Header = ()=>{
 
     <div className="nav-items">
       <ul>
+        <li>onlineStatus :{onlineStatus ? "🟩":"🔴"}</li>
         <li><Link to ='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
         <li><Link to='/contact'>Contact-us</Link></li>
+        <li><Link to='/grocery'>Grocery</Link></li>
         <li>Cart</li>
         <button className="login" onClick={()=>{
           btnNameReact ==="Login"
--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,44 +1,19 @@
-import { useState } from "react";
-import { useEffect } from "react";
+
 import Shimmer from './Shimmer';
 import { useParams } from "react-router-dom";
+import RestaurantMenu from "../../utils/useRestaurantMenu";
 
 
 const RestaurantMenu = ()=>{ 
    
-  const [resInfo ,setresInfo]= useState(null);
-
   const {resId} = useParams();
- // console.log(params);
-  useEffect(()=>{
-    fetchMenu();
-  },[])
-
-  const fetchMenu = async() =>{
-    try{
-      let response = await  fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6369411&lng=77.2056647&restaurantId="+resId+"&catalog_qa=undefined&metaData=%7B%22type%22%3A%22RESTAURANT%22%2C%22data%22%3A%7B%22parentId%22%3A27714%2C%22primaryRestaurantId%22%3A322437%2C%22cloudinaryId%22%3A%22a16yrlrtgmi37nnagibq%22%2C%22brandId%22%3A27714%2C%22enabled_flag%22%3A1%7D%2C%22businessCategory%22%3A%22SWIGGY_FOOD%22%2C%22displayLabel%22%3A%22Restaurant%22%7D&submitAction=SUGGESTION");
-
-      let json = await response.json();
-
-      //console.log(json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards[0].card.info.name)
-
-
-    //  console.log(json.data.cards[0].card.card.text)
-
-      setresInfo(json.data);
-    }catch(err){
-     console.log("error is " , err);
-    }
-
-    
-  }
+  const resInfo = useRestaurantMenu(resId);
+ 
   if(resInfo===null) return <Shimmer/>;
-  //const {name} = resInfo.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
+  
   const {name} = resInfo.cards[2].card.card.info
   console.log(name)
   const itemCards = resInfo?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[3]?.card?.card?.itemCards || [];
-
-// console.log(itemCards)
 
 return (
   <div>
